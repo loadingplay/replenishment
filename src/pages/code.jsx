18247c768531142da3cc501e98da2f952f6011d1
Ulises <texanico@gmail.com>
2019-07-16 21:50:56 -0400

    UseStorage

--- src/pages/code.jsx
+++ src/pages/code.jsx
@@ -1,11 +1,19 @@
-import React, { Component } from 'react'
+import React, { useEffect } from 'react'
 import { navigate } from 'gatsby'
 import loginService from '../services/loginService'
+import useLocalStorage from '../hooks/useLocalStorage'
 
-export default class code extends Component {
+function saveToken(token){
+    useLocalStorage('token', token); // Hook
+}
 
-    componentDidMount(){
-        let params = this.props.location.href.split("?")[1].split("&");
+const code = (props) => {
+
+    const [token, setToken] = useLocalStorage('token', token);
+
+    useEffect(() => {
+        // code to run on component mount
+        let params = props.location.href.split("?")[1].split("&");
         let code = params[1].match(/code=(.*)/)[1];
         
         let objToken = {
@@ -18,19 +26,20 @@ export default class code extends Component {
 
         loginService.getToken(objToken).then((data) => {
             let token = data.access_token
+            setToken(token)
             loginService.getUser(token).then((data) => {
                 console.log(data);
                 navigate(`/dashboard/`)
             })
         });
-    }
+      }, [])
 
-    render() {
 
         return (
             <div>
                 <h1>GENERANDO CODIGO</h1>
             </div>
         )
-    }
 }
+
+export default code;
