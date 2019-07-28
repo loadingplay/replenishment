d8ce7b14077c26442b5942b9bff57896eaf62204
Ulises <texanico@gmail.com>
2019-07-28 14:25:52 -0400

    sku search

--- src/components/dashboard/stock.jsx
+++ src/components/dashboard/stock.jsx
@@ -6,89 +6,92 @@ import authService from '../../services/authService';
 import stockService from '../../services/stockService';
 
 const stock = (props) => {
-    const [data, dataSet] = useState([]);
-    const [offset, setOffset] = useState(0);
-    const [currentPage, setCurrentPage] = useState(1);
-    const [currentData, setCurrentData] = useState([]);
-    const pageLimit = 10;
+  const [data, dataSet] = useState([]);
+  const [offset, setOffset] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [currentData, setCurrentData] = useState([]);
+  const [sku, setSku] = useState('');
+  const token = authService.getCurrentToken();
+  const pageLimit = 10;
 
-    async function fetchPagedData() {
-      let token = authService.getCurrentToken();
-      console.log('Pagina Actual', currentPage)
-      console.log('Id actual', props.cellarId)
-      let stocksPaged = await stockService.getSuggestedProducts(props.cellarId, token);
-      console.log(stocksPaged);
-      dataSet(stocksPaged.data.replenishments);
-    }
-  
-    useEffect(() => {
-      fetchPagedData();
-    }, [props.cellarId])
+  async function fetchPagedData() {
+    let stocksPaged = await stockService.getSuggestedProducts(props.cellarId, token);
+    dataSet(stocksPaged.data.replenishments);
+  }
 
-    useEffect(() => {
-      setCurrentData(data ? data.slice(offset, offset + pageLimit) : 0);
-    }, [offset, data]);
-    
-    console.log(currentData);
-    
-    return (
-      <section className="stores_wrapper">
-        <table className="table table-hover table-borderless">
-          <thead>
-            <tr className="table-info">
-              <th scope="col">sku</th>
-              <th scope="col">nombre producto</th>
-              <th scope="col">stock en bodega</th>
-              <th scope="col">stock en tienda</th>
-              <th scope="col">sugerido</th>
-              <th scope="col">picker</th>
+  useEffect(() => {
+    fetchPagedData();
+  }, [props.cellarId])
+
+  useEffect(() => {
+    setCurrentData(data ? data.slice(offset, offset + pageLimit) : 0);
+  }, [offset, data]);
+
+  async function handleSearch(evt) {
+    console.log(evt.target.value);
+    let sku = evt.target.value;
+    let currentInventory = await stockService.getCurrentInventory(props.cellarId, token, sku);
+    console.log(currentInventory);
+  }
+
+  return (
+    <section className="stores_wrapper">
+      <table className="table table-hover table-borderless">
+        <thead>
+          <tr className="table-info">
+            <th scope="col">sku</th>
+            <th scope="col">nombre producto</th>
+            <th scope="col">stock en bodega</th>
+            <th scope="col">stock en tienda</th>
+            <th scope="col">sugerido</th>
+            <th scope="col">picker</th>
+          </tr>
+        </thead>
+        <tbody>
+          {currentData ? currentData.map((item, i) =>
+            <tr key={i}>
+              <th scope="row">{item.sku}</th>
+              <td>{item.site_name}</td>
+              <td></td>
+              <td></td>
+              <td>{item.suggested ? 'Sí' : 'No'}</td>
+              <td></td>
             </tr>
-          </thead>
-          <tbody>
-            {currentData ? currentData.map((item, i) =>
-              <tr key={i}>
-                <th scope="row">{item.sku}</th>
-                <td>{item.site_name}</td>
-                <td></td>
-                <td></td>
-                <td>{item.suggested ? 'Sí' : 'No'}</td>
-                <td></td>
-              </tr>
-            ) : <tr><td>Buscando...</td></tr>}
-          </tbody>
-        </table>
-        <Paginator
-          totalRecords={data ? data.length : 1}
-          pageLimit={4}
-          pageNeighbours={1}
-          setOffset={setOffset}
-          currentPage={currentPage}
-          setCurrentPage={setCurrentPage}
-        />
-        <section className="search_actions">
+          ) : <tr><td>Buscando...</td></tr>}
+        </tbody>
+      </table>
+      <Paginator
+        totalRecords={data ? data.length : 1}
+        pageLimit={4}
+        pageNeighbours={1}
+        setOffset={setOffset}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      />
+      <section className="search_actions">
+        <div>
+          <input type="text" placeholder="ingrese sku" onChange={handleSearch} />
+        </div>
+        <div className="list_btn">
           <div>
-            <input type="text" placeholder="ingrese sku" />
+            <button type="button" className="btn btn-success">
+              imprimir
+              </button>
           </div>
-          <div className="list_btn">
-            <div>
-              <button type="button" className="btn btn-success">
-                imprimir
+          <div>
+            <button type="button" className="btn btn-primary">
+              generar guia de despacho
               </button>
-            </div>
-            <div>
-              <button type="button" className="btn btn-primary">
-                generar guia de despacho
-              </button>
-            </div>
-            <div>
-              <button type="button" className="btn btn-danger" onClick={authService.logout}>
-                cerrar sesión
-              </button>
-            </div>
           </div>
-        </section>
+          <div>
+            <button type="button" className="btn btn-danger" onClick={authService.logout}>
+              cerrar sesión
+              </button>
+          </div>
+        </div>
       </section>
-    )
+    </section>
+  )
 }
 
 export default stock;
--- src/services/stockService.js
+++ src/services/stockService.js
@@ -55,5 +55,24 @@ export default {
             }
             return objResponse;
         });
+    },
+    getCurrentInventory(id, token, sku){
+        token = token.replace(/^"(.*)"$/, '$1');
+        id = parseInt(id);
+        return fetch(`https://apibodegas.loadingplay.com/v1/cellar/${id}/product/${sku}`, {
+        method: 'GET',
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            'Authorization': 'Bearer ' + token,
+            }
+        }).then(function (response) {
+            return response.json();
+        }).then(function (responseData) {
+            let objResponse = {
+                cellarId: id,
+                data: responseData
+            }
+            return objResponse;
+        });
     }
 }
