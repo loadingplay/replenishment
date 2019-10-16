413c7a0cbb9e009d6ac2ddda91bbdf50c0a0419e
Ricardo Silva <ricardo@loadingplay.com>
2019-10-16 20:10:45 -0300

    added more tests

--- src/library/components/__tests__/searchinput.tests.jsx
+++ src/library/components/__tests__/searchinput.tests.jsx
@@ -8,4 +8,45 @@ describe("SearchInput", () => {
 
     expect(wrapper).toMatchSnapshot("default");
   });
+
+  test("it should change search type", () => {
+    const onTypeChange = jest.fn();
+    const wrapper = shallow(<SearchInput onTypeChange={onTypeChange} />);
+
+    expect(wrapper.instance().state.type).toEqual("search");
+    expect(onTypeChange).toHaveBeenCalledTimes(0);
+    wrapper.instance().changeType();
+    expect(wrapper.instance().state.type).toEqual("barcode");
+    expect(onTypeChange).toHaveBeenCalledTimes(1);
+    wrapper.instance().changeType();
+    expect(wrapper.instance().state.type).toEqual("search");
+    expect(onTypeChange).toHaveBeenCalledTimes(2);
+  });
+
+  test("it should handle when keydown", () => {
+    const wrapper = shallow(<SearchInput />);
+    const e = { key: "Enter", target: { select: jest.fn() } };
+
+    wrapper.instance().textInput = { current: { focus: jest.fn() } };
+
+    expect(wrapper.instance().textInput.current.focus).toHaveBeenCalledTimes(0);
+    wrapper.instance().handleKeyDown({ key: "t" });
+    expect(wrapper.instance().textInput.current.focus).toHaveBeenCalledTimes(1);
+
+    expect(e.target.select).toHaveBeenCalledTimes(0);
+    wrapper.instance().handleKeyDown(e);
+    expect(e.target.select).toHaveBeenCalledTimes(1);
+  });
+
+  test("it should handle changes", () => {
+    const wrapper = shallow(<SearchInput />);
+
+    wrapper.instance().handleSearch = jest.fn();
+
+    expect(wrapper.instance().state.value).toEqual("");
+    wrapper.instance().handleChange({ target: { value: "test" } });
+    expect(wrapper.instance().state.value).toEqual("test");
+    expect(wrapper.instance().handleSearch).toHaveBeenCalledWith("test");
+  });
+
 });
